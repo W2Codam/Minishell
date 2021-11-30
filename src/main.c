aa12e2573673a3e555b8093d6dc9508b34073b26
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2021-11-30 14:44:18 +0100

    Move main

--- src/main.c
+++ src/main.c
@@ -6,11 +6,13 @@
 /*   By: lde-la-h <lde-la-h@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/30 13:23:11 by lde-la-h      #+#    #+#                 */
-/*   Updated: 2021/11/30 13:23:46 by lde-la-h      ########   odam.nl         */
+/*   Updated: 2021/11/30 13:33:56 by lde-la-h      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int argc, char const **argv, char const**envp)
+#include "libft.h"
+
+t_i32	main(t_i32 argc, char const **argv, char const **envp)
 {
 	return (0);
 }
